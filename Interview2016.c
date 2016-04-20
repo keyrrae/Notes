baa14d5f8fca5a5cb5cfdd8436e05f708b7b777c
Xuan Wang <xuan-wang@outlook.com>
2016-04-19 22:03:22 -0700

    Update Interview2016.c

--- Interview2016.c
+++ Interview2016.c
@@ -364,5 +364,72 @@ A question about what the main function in C does and why it is important to kno
 Print a string and print a string in reverse order?
 In-order traverse a binary search tree.
 
+
+#include <stdio.h>
+
+/* Write a quick program to tell me the size of this structure: */
+
+struct s {
+   double f;
+   int i;
+   char c[3];
+   void *p;
+   int x[0];
+}
+
+int main( int argc, char ** argv ) {
+
+}
+
+============================================================================
+/* What does this do?
+#define ABC(x) aaa ## x
+
+ABC(b)
+*/
+
+============================================================================
+/* Find the size */
+int main() {
+   const char *s1 = "hello";
+   char s2[] = "world";
+
+   printf( "sizeof s1 : %d sizeof s2 : %d\n", sizeof(s1), sizeof(s2) );
+   printf( "lenof s2 : %d\n", strlen(s2) );
+}
+
+============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argc, char* argv[] ){
+    char* s1 = "STRING1";
+    char s2[] = "STRING2";
+    char *s3 = NULL;
+
+    s1[ 1 ] = 'P';
+    s2[ 1 ] = 'P';
+
+    s3 = malloc(5);
+    strcpy(s3, "test");
+    s3[1]= 'P';
+}
+
+============================================================================
+/* What is the difference between these variables? */                                                           
+
+int i1;
+int j=0;
+
+int main() {
+   const char *s1 = "hello world";
+   char s2[] = "hellow world";
+   static int i2;
+   static int k=1;
+}
+
+
 Implement a stack in my favorite language
 2) OS and computer systems questions: x86 stack frame, Linux kernel (?)
